c9885073507c211e4bf4c5a55bfd09b029f2ea16
Theara Ya <theara.ya@outlook.com>
2023-11-05 16:16:28 -0500

    Config

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from config import Config
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
+    app.config.from_object(config_class)
+    
     return app

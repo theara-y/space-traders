c2eab00e83e4d084f06faaa2d527871549e1ba2f
Theara Ya <theara.ya@outlook.com>
2023-11-05 18:43:07 -0500

    Enable database migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,22 @@
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
     db.init_app(app)
+    migrate.init_app(app, db)
 
     # Register blueprints
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
-    
+
     from app.users import bp as users_bp
     app.register_blueprint(users_bp, url_prefix='/users')
 
--- app/models.py
+++ app/models.py
@@ -4,4 +4,4 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), index=True, unique=True)
-    password = db.Column(db.String(128))
+    password = db.Column(db.LargeBinary)
